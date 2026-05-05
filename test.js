a1f0be9bc24d289d3a4c1eea646a4bb5eb0926e8
AlexJ256 <alexxj2000@gmail.com>
2026-05-05 12:03:20 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,20 @@
 
 const sum = require('./sum.js');
 
-  test('debe sumar correctamente', () => {
-    assert.strictEqual(sum(10, 5), 15);
-  });
-  test('debe sumar correctamente', () => {
-    assert.strictEqual(sum(10, 5), 5);
-  });
-  test('debe sumar correctamente', () => {
-    assert.strictEqual(sum(-5, 5), 0);
-  });
-  test('debe sumar correctamente', () => {
-    assert.strictEqual(sum(-10, -5), -15);
-  });
+if (sum(2,3) !==5){
+  throw new Error("Test failed: sum(2,3) deberia ser 5);
+}
+console.log("Test Passed");
+if (sum(10, 5) !==15){
+  throw new Error("Test failed: sum(2,3) deberia ser 5);
+}
+console.log("Test Passed");
+if (sum(-5, 5) !==0){
+  throw new Error("Test failed: sum(2,3) deberia ser 5);
+}
+console.log("Test Passed");
+
+if (sum(-10, -5) !==-15){
+  throw new Error("Test failed: sum(2,3) deberia ser 5);
+}
+console.log("Test Passed");
