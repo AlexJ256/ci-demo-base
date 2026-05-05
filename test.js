57ae6bf5c30e15ad1f7a84a6f4209149c95edb73
AlexJ256 <alexxj2000@gmail.com>
2026-05-05 11:44:12 -0400

    Update test.js

--- test.js
+++ test.js
@@ -2,20 +2,15 @@ import test from 'node:test';
 import assert from 'node:assert';
 import sum from './sum.js';
 
-test('Pruebas de la calculadora', async (t) => {
-  
-  await t.test('debe sumar correctamente', () => {
+  test('debe sumar correctamente', () => {
     assert.strictEqual(sum(10, 5), 15);
   });
-
-  await t.test('debe sumar correctamente', () => {
+  test('debe sumar correctamente', () => {
     assert.strictEqual(sum(10, 5), 5);
   });
-  await t.test('debe sumar correctamente', () => {
+  test('debe sumar correctamente', () => {
     assert.strictEqual(sum(-5, 5), 0);
   });
-  await t.test('debe sumar correctamente', () => {
+  test('debe sumar correctamente', () => {
     assert.strictEqual(sum(-10, -5), -15);
   });
-  
-});
