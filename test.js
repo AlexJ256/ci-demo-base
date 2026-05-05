af22b531349c3744d7137517aec0178c25b393d7
AlexJ256 <alexxj2000@gmail.com>
2026-05-05 12:04:42 -0400

    Update test.js

--- test.js
+++ test.js
@@ -2,19 +2,19 @@
 const sum = require('./sum.js');
 
 if (sum(2,3) !==5){
-  throw new Error("Test failed: sum(2,3) deberia ser 5);
+  throw new Error("Test failed: sum(2,3) deberia ser 5");
 }
 console.log("Test Passed");
 if (sum(10, 5) !==15){
-  throw new Error("Test failed: sum(2,3) deberia ser 5);
+  throw new Error("Test failed: sum(10, 5) deberia ser 5");
 }
 console.log("Test Passed");
 if (sum(-5, 5) !==0){
-  throw new Error("Test failed: sum(2,3) deberia ser 5);
+  throw new Error("Test failed: sum(-5, 5) deberia ser 5");
 }
 console.log("Test Passed");
 
 if (sum(-10, -5) !==-15){
-  throw new Error("Test failed: sum(2,3) deberia ser 5);
+  throw new Error("Test failed: sum(-10, -5) deberia ser 5");
 }
 console.log("Test Passed");
